d49ab691ea4d370a19d33f79b629346a48367ea1
Danny Tsang <danny@tsang.uk>
2022-07-10 00:35:58 +0100

    Bump Deebot integration version.

--- custom_components/deebot/const.py
+++ custom_components/deebot/const.py
@@ -26,8 +26,8 @@ from homeassistant.const import CONF_PASSWORD, CONF_USERNAME, CONF_VERIFY_SSL
 
 ################################
 # Do not change! Will be set by release workflow
-INTEGRATION_VERSION = "1.3.1"
-MIN_REQUIRED_HA_VERSION = "2022.5.0b0"
+INTEGRATION_VERSION = "1.4.1"
+MIN_REQUIRED_HA_VERSION = "2022.7.0b0"
 ################################
 
 # Values below can be changed
