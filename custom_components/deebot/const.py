04a4342755aa1d82cf0628c9f60136488acbc918
Danny Tsang <danny@tsang.uk>
2022-11-03 01:38:31 +0000

    Bump deetbot integration version.

--- custom_components/deebot/const.py
+++ custom_components/deebot/const.py
@@ -1,5 +1,5 @@
 """Const module."""
-from typing import Mapping
+from collections.abc import Mapping
 
 from deebot_client.events import (
     BatteryEvent,
@@ -26,8 +26,8 @@ from homeassistant.const import CONF_PASSWORD, CONF_USERNAME, CONF_VERIFY_SSL
 
 ################################
 # Do not change! Will be set by release workflow
-INTEGRATION_VERSION = "1.4.1"
-MIN_REQUIRED_HA_VERSION = "2022.7.0b0"
+INTEGRATION_VERSION = "1.5.0"
+MIN_REQUIRED_HA_VERSION = "2022.10.0b0"
 ################################
 
 # Values below can be changed
