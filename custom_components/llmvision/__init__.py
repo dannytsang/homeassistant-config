6fed5c7636318ad203f2cd1fb0d3a66bd52889fd
Danny Tsang <danny@tsang.uk>
2024-07-28 10:56:28 +0100

    Bump LLM vision integration version.

---
+++ custom_components/llmvision/__init__.py
@@ -0,0 +1,302 @@
+# Declare variables
+from .const import (
+    DOMAIN,
+    CONF_OPENAI_API_KEY,
+    CONF_ANTHROPIC_API_KEY,
+    CONF_GOOGLE_API_KEY,
+    CONF_LOCALAI_IP_ADDRESS,
+    CONF_LOCALAI_PORT,
+    CONF_OLLAMA_IP_ADDRESS,
+    CONF_OLLAMA_PORT,
+    PROVIDER,
+    MAXTOKENS,
+    TARGET_WIDTH,
+    MODEL,
+    MESSAGE,
+    IMAGE_FILE,
+    IMAGE_ENTITY,
+    TEMPERATURE,
+    DETAIL,
+    INCLUDE_FILENAME,
+    ERROR_OPENAI_NOT_CONFIGURED,
+    ERROR_ANTHROPIC_NOT_CONFIGURED,
+    ERROR_GOOGLE_NOT_CONFIGURED,
+    ERROR_LOCALAI_NOT_CONFIGURED,
+    ERROR_OLLAMA_NOT_CONFIGURED,
+    ERROR_NO_IMAGE_INPUT
+)
+from .request_handlers import RequestHandler
+import base64
+import io
+import os
+import logging
+from homeassistant.helpers.network import get_url
+from homeassistant.core import SupportsResponse
+from homeassistant.exceptions import ServiceValidationError
+from PIL import Image
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup_entry(hass, entry):
+    """Save llmvision config entry in hass.data"""
+    # Get all entries from config flow
+    openai_api_key = entry.data.get(CONF_OPENAI_API_KEY)
+    anthropic_api_key = entry.data.get(CONF_ANTHROPIC_API_KEY)
+    google_api_key = entry.data.get(CONF_GOOGLE_API_KEY)
+    localai_ip_address = entry.data.get(CONF_LOCALAI_IP_ADDRESS)
+    localai_port = entry.data.get(CONF_LOCALAI_PORT)
+    ollama_ip_address = entry.data.get(CONF_OLLAMA_IP_ADDRESS)
+    ollama_port = entry.data.get(CONF_OLLAMA_PORT)
+
+    # Ensure DOMAIN exists in hass.data
+    if DOMAIN not in hass.data:
+        hass.data[DOMAIN] = {}
+
+    # Merge the new data with the existing data
+    hass.data[DOMAIN].update({
+        key: value
+        for key, value in {
+            CONF_OPENAI_API_KEY: openai_api_key,
+            CONF_ANTHROPIC_API_KEY: anthropic_api_key,
+            CONF_GOOGLE_API_KEY: google_api_key,
+            CONF_LOCALAI_IP_ADDRESS: localai_ip_address,
+            CONF_LOCALAI_PORT: localai_port,
+            CONF_OLLAMA_IP_ADDRESS: ollama_ip_address,
+            CONF_OLLAMA_PORT: ollama_port
+        }.items()
+        if value is not None
+    })
+
+    return True
+
+
+def validate(mode, api_key, base64_images, ip_address=None, port=None):
+    """Validate the configuration for the component
+
+    Args:
+        mode (string): "OpenAI" or "LocalAI"
+        api_key (string): OpenAI API key
+        ip_address (string): LocalAI server IP address
+        port (string): LocalAI server port
+
+    Raises:
+        ServiceValidationError: if configuration is invalid
+    """
+    # Checks for OpenAI
+    if mode == 'OpenAI':
+        if not api_key:
+            raise ServiceValidationError(ERROR_OPENAI_NOT_CONFIGURED)
+    # Checks for Anthropic
+    elif mode == 'Anthropic':
+        if not api_key:
+            raise ServiceValidationError(ERROR_ANTHROPIC_NOT_CONFIGURED)
+    elif mode == 'Google':
+        if not api_key:
+            raise ServiceValidationError(ERROR_GOOGLE_NOT_CONFIGURED)
+    # Checks for LocalAI
+    elif mode == 'LocalAI':
+        if not ip_address or not port:
+            raise ServiceValidationError(ERROR_LOCALAI_NOT_CONFIGURED)
+    # Checks for Ollama
+    elif mode == 'Ollama':
+        if not ip_address or not port:
+            raise ServiceValidationError(ERROR_OLLAMA_NOT_CONFIGURED)
+    # File path validation
+    if base64_images == []:
+        raise ServiceValidationError(ERROR_NO_IMAGE_INPUT)
+
+
+def setup(hass, config):
+    async def image_analyzer(data_call):
+        """Handle the service call to analyze an image with LLM Vision
+
+        Returns:
+            json: response_text
+        """
+        # HELPERS
+        async def encode_image(image_path=None, image_data=None):
+            """Encode image as base64
+
+            Args:
+                image_path (string): path where image is stored e.g.: "/config/www/tmp/image.jpg"
+
+            Returns:
+                string: image encoded as base64
+            """
+            loop = hass.loop
+            if image_path:
+                # Open the image file
+                img = await loop.run_in_executor(None, Image.open, image_path)
+                with img:
+                    # calculate new height based on aspect ratio
+                    width, height = img.size
+                    aspect_ratio = width / height
+                    target_height = int(target_width / aspect_ratio)
+
+                    # Resize the image only if it's larger than the target size
+                    if width > target_width or height > target_height:
+                        img = img.resize((target_width, target_height))
+
+                    # Convert the image to base64
+                    img_byte_arr = io.BytesIO()
+                    img.save(img_byte_arr, format='JPEG')
+                    base64_image = base64.b64encode(
+                        img_byte_arr.getvalue()).decode('utf-8')
+
+            elif image_data:
+                # Convert the image to base64
+                img_byte_arr = io.BytesIO()
+                img_byte_arr.write(image_data)
+                img = await loop.run_in_executor(None, Image.open, img_byte_arr)
+                with img:
+                    # calculate new height based on aspect ratio
+                    width, height = img.size
+                    aspect_ratio = width / height
+                    target_height = int(target_width / aspect_ratio)
+
+                    if width > target_width or height > target_height:
+                        img = img.resize((target_width, target_height))
+
+                    img.save(img_byte_arr, format='JPEG')
+                    base64_image = base64.b64encode(
+                        img_byte_arr.getvalue()).decode('utf-8')
+
+            return base64_image
+
+        # Read from configuration (hass.data)
+        localai_ip_address = hass.data.get(
+            DOMAIN, {}).get(CONF_LOCALAI_IP_ADDRESS)
+        localai_port = hass.data.get(DOMAIN, {}).get(CONF_LOCALAI_PORT)
+        ollama_ip_address = hass.data.get(
+            DOMAIN, {}).get(CONF_OLLAMA_IP_ADDRESS)
+        ollama_port = hass.data.get(DOMAIN, {}).get(CONF_OLLAMA_PORT)
+
+        # Read data from service call
+        mode = str(data_call.data.get(PROVIDER))
+        message = str(data_call.data.get(MESSAGE)[0:2000])
+        image_paths = data_call.data.get(IMAGE_FILE, "").split(
+            "\n") if data_call.data.get(IMAGE_FILE) else None
+        image_entities = data_call.data.get(IMAGE_ENTITY)
+        target_width = data_call.data.get(TARGET_WIDTH, 1280)
+        temperature = float(data_call.data.get(TEMPERATURE, 0.5))
+        max_tokens = int(data_call.data.get(MAXTOKENS, 100))
+        detail = str(data_call.data.get(DETAIL, "auto"))
+        include_filename = data_call.data.get(INCLUDE_FILENAME, False)
+
+        base64_images = []
+        filenames = []
+
+        client = RequestHandler(hass,
+                                message=message,
+                                max_tokens=max_tokens,
+                                temperature=temperature,
+                                detail=detail)
+
+        # If image_paths is not empty, encode the images as base64 and add them to the client
+        if image_paths:
+            for image_path in image_paths:
+                try:
+                    image_path = image_path.strip()
+                    if include_filename and os.path.exists(image_path):
+                        client.add_image(
+                            base64_image=await encode_image(image_path=image_path),
+                            filename=image_path.split('/')[-1].split('.')[-2]
+                        )
+                    elif os.path.exists(image_path):
+                        client.add_image(
+                            base64_image=await encode_image(image_path=image_path),
+                            filename=""
+                        )
+                    if not os.path.exists(image_path):
+                        raise ServiceValidationError(
+                            f"File {image_path} does not exist")
+                except Exception as e:
+                    raise ServiceValidationError(f"Error: {e}")
+
+        # If image_entities is not empty, fetch, encode the images as base64 and add them to the client
+        if image_entities:
+            for image_entity in image_entities:
+                try:
+                    base_url = get_url(hass)
+                    image_url = base_url + hass.states.get(image_entity).attributes.get('entity_picture')
+                    image_data = await client.fetch(image_url)
+                
+
+                    # If entity snapshot requested, use entity name as 'filename'
+                    if include_filename:
+                        entity_name = hass.states.get(image_entity).attributes.get('friendly_name')
+
+                        client.add_image(
+                            base64_image=await encode_image(image_data=image_data),
+                            filename=entity_name
+                        )
+                    else:
+                        client.add_image(
+                            base64_image=await encode_image(image_data=image_data),
+                            filename=""
+                        )
+                except AttributeError as e:
+                    raise ServiceValidationError(
+                        f"Entity {image_entity} does not exist")
+                
+        _LOGGER.debug(f"Base64 Images: {client.get_images()}")
+
+        # Validate configuration and input data, make the call
+        if mode == 'OpenAI':
+            api_key = hass.data.get(DOMAIN).get(CONF_OPENAI_API_KEY)
+            validate(mode=mode,
+                     api_key=api_key,
+                     base64_images=client.get_images())
+            model = str(data_call.data.get(MODEL, "gpt-4o-mini"))
+            response_text = await client.openai(model=model, api_key=api_key)
+        elif mode == 'Anthropic':
+            api_key = hass.data.get(DOMAIN).get(CONF_ANTHROPIC_API_KEY)
+            validate(mode=mode,
+                     api_key=api_key,
+                     base64_images=client.get_images())
+            model = str(data_call.data.get(
+                MODEL, "claude-3-5-sonnet-20240620"))
+            response_text = await client.anthropic(model=model, api_key=api_key)
+        elif mode == 'Google':
+            api_key = hass.data.get(DOMAIN).get(CONF_GOOGLE_API_KEY)
+            validate(mode=mode, api_key=api_key,
+                     base64_images=client.get_images())
+            model = str(data_call.data.get(
+                MODEL, "gemini-1.5-flash-latest"))
+            response_text = await client.google(model=model, api_key=api_key)
+        elif mode == 'LocalAI':
+            validate(mode=mode,
+                     api_key=None,
+                     base64_images=client.get_images(),
+                     ip_address=localai_ip_address,
+                     port=localai_port)
+            model = str(data_call.data.get(MODEL, "gpt-4-vision-preview"))
+            response_text = await client.localai(model=model,
+                                                 ip_address=localai_ip_address,
+                                                 port=localai_port)
+        elif mode == 'Ollama':
+            validate(mode=mode,
+                     api_key=None,
+                     base64_images=client.get_images(),
+                     ip_address=ollama_ip_address,
+                     port=ollama_port)
+            model = str(data_call.data.get(MODEL, "llava"))
+            response_text = await client.ollama(model=model,
+                                                ip_address=ollama_ip_address,
+                                                port=ollama_port)
+
+        # close the RequestHandler and return response_text
+        await client.close()
+        return {"response_text": response_text}
+
+    async def video_analyzer(data_call):
+       """Handle the service call to analyze a video (future implementation)"""
+       pass 
+
+    hass.services.register(
+        DOMAIN, "image_analyzer", image_analyzer,
+        supports_response=SupportsResponse.ONLY
+    )
+
+    return True
---
+++ custom_components/llmvision/request_handlers.py
@@ -0,0 +1,337 @@
+from homeassistant.exceptions import ServiceValidationError
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+import logging
+import re
+from .const import (
+    VERSION_ANTHROPIC,
+    ENDPOINT_OPENAI,
+    ENDPOINT_ANTHROPIC,
+    ENDPOINT_GOOGLE,
+    ENDPOINT_LOCALAI,
+    ENDPOINT_OLLAMA
+)
+
+_LOGGER = logging.getLogger(__name__)
+base64_pattern = re.compile(r'([A-Za-z0-9+/=]{1000,})')
+
+
+def sanitize_data(data):
+    """Remove base64 image data from request data to reduce log size"""
+    if isinstance(data, dict):
+        return {key: sanitize_data(value) for key, value in data.items()}
+    elif isinstance(data, list):
+        return [sanitize_data(item) for item in data]
+    elif isinstance(data, str) and base64_pattern.match(data):
+        return '<base64_image>'
+    else:
+        return data
+
+
+class RequestHandler:
+    def __init__(self, hass, message, max_tokens, temperature, detail):
+        self.session = async_get_clientsession(hass)
+        self.message = message
+        self.max_tokens = max_tokens
+        self.temperature = temperature
+        self.detail = detail
+        self.base64_images = []
+        self.filenames = []
+
+    def add_image(self, base64_image, filename):
+        self.base64_images.append(base64_image)
+        self.filenames.append(filename)
+
+    def get_images(self):
+        return self.base64_images
+
+    async def openai(self, model, api_key):
+        from .const import ENDPOINT_OPENAI
+        # Set headers and payload
+        headers = {'Content-type': 'application/json',
+                   'Authorization': 'Bearer ' + api_key}
+        data = {"model": model,
+                "messages": [{"role": "user", "content": [
+                ]}],
+                "max_tokens": self.max_tokens,
+                "temperature": self.temperature
+                }
+
+        # Add the images to the request
+        for image, filename in zip(self.base64_images, self.filenames):
+            tag = ("Image " + str(self.base64_images.index(image) + 1)
+                   ) if filename == "" else filename
+            data["messages"][0]["content"].append(
+                {"type": "text", "text": tag + ":"})
+            data["messages"][0]["content"].append(
+                {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{image}", "detail": self.detail}})
+
+        # append the message to the end of the request
+        data["messages"][0]["content"].append(
+            {"type": "text", "text": self.message}
+        )
+
+        response = await self._post(
+            url=ENDPOINT_OPENAI, headers=headers, data=data)
+
+        response_text = response.get(
+            "choices")[0].get("message").get("content")
+        return response_text
+
+    async def anthropic(self, model, api_key):
+        from .const import ENDPOINT_ANTHROPIC
+        # Set headers and payload
+        headers = {'content-type': 'application/json',
+                   'x-api-key': api_key,
+                   'anthropic-version': VERSION_ANTHROPIC}
+        data = {"model": model,
+                "messages": [
+                    {"role": "user", "content": []}
+                ],
+                "max_tokens": self.max_tokens,
+                "temperature": self.temperature
+                }
+
+        # Add the images to the request
+        for image, filename in zip(self.base64_images, self.filenames):
+            tag = ("Image " + str(self.base64_images.index(image) + 1)
+                   ) if filename == "" or not filename else filename
+            data["messages"][0]["content"].append(
+                {
+                    "type": "text",
+                    "text": tag + ":"
+                })
+            data["messages"][0]["content"].append(
+                {"type": "image", "source":
+                    {"type": "base64",
+                     "media_type": "image/jpeg",
+                     "data": f"{image}"
+                     }
+                 }
+            )
+
+        # append the message to the end of the request
+        data["messages"][0]["content"].append(
+            {"type": "text", "text": self.message}
+        )
+
+        response = await self._post(
+            url=ENDPOINT_ANTHROPIC, headers=headers, data=data)
+
+        response_text = response.get("content")[0].get("text")
+        return response_text
+
+    async def google(self, model, api_key):
+        from .const import ENDPOINT_GOOGLE
+        # Set headers and payload
+        headers = {'content-type': 'application/json'}
+        data = {"contents": [
+        ],
+            "generationConfig": {
+                "maxOutputTokens": self.max_tokens,
+                "temperature": self.temperature
+        }
+        }
+
+        # Add the images to the request
+        for image, filename in zip(self.base64_images, self.filenames):
+            tag = ("Image " + str(self.base64_images.index(image) + 1)
+                   ) if filename == "" or not filename else filename
+            data["contents"].append(
+                {
+                    "role": "user",
+                    "parts": [
+                        {
+                            "text": tag + ":"
+                        },
+                        {
+                            "inline_data": {
+                                "mime_type": "image/jpeg",
+                                "data": image
+                            }
+                        }
+                    ]
+                }
+            )
+
+        # append the message to the end of the request
+        data["contents"].append(
+            {"role": "user",
+             "parts": [{"text": self.message}
+                       ]
+             }
+        )
+
+        response = await self._post(
+            url=ENDPOINT_GOOGLE.format(model=model, api_key=api_key), headers=headers, data=data)
+
+        response_text = response.get("candidates")[0].get(
+            "content").get("parts")[0].get("text")
+        return response_text
+
+    async def localai(self, model, ip_address, port):
+        from .const import ENDPOINT_LOCALAI
+        data = {"model": model,
+                "messages": [{"role": "user", "content": [
+                ]}],
+                "max_tokens": self.max_tokens,
+                "temperature": self.temperature
+                }
+        for image, filename in zip(self.base64_images, self.filenames):
+            tag = ("Image " + str(self.base64_images.index(image) + 1)
+                   ) if filename == "" or not filename else filename
+            data["messages"][0]["content"].append(
+                {"type": "text", "text": tag + ":"})
+            data["messages"][0]["content"].append(
+                {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{image}"}})
+
+        # append the message to the end of the request
+        data["messages"][0]["content"].append(
+            {"type": "text", "text": self.message}
+        )
+
+        response = await self._post(
+            url=ENDPOINT_LOCALAI.format(ip_address=ip_address, port=port), headers={}, data=data)
+
+        response_text = response.get(
+            "choices")[0].get("message").get("content")
+        return response_text
+
+    async def ollama(self, model, ip_address, port):
+        from .const import ENDPOINT_OLLAMA
+        data = {
+            "model": model,
+            "messages": [],
+            "stream": False,
+            "options": {
+                "max_tokens": self.max_tokens,
+                "temperature": self.temperature
+            }
+        }
+
+        for image, filename in zip(self.base64_images, self.filenames):
+            tag = ("Image " + str(self.base64_images.index(image) + 1)
+                   ) if filename == "" or not filename else filename
+            image_message = {
+                "role": "user",
+                "content": tag + ":",
+                "images": [image]
+            }
+            data["messages"].append(image_message)
+        # append to the end of the request
+        prompt_message = {
+            "role": "user",
+            "content": self.message
+        }
+        data["messages"].append(prompt_message)
+
+        response = await self._post(url=ENDPOINT_OLLAMA.format(ip_address=ip_address, port=port), headers={}, data=data)
+        response_text = response.get("message").get("content")
+        return response_text
+
+    async def _post(self, url, headers, data):
+        """Post data to url and return response data"""
+        _LOGGER.info(f"Request data: {sanitize_data(data)}")
+        try:
+            response = await self.session.post(url, headers=headers, json=data)
+        except Exception as e:
+            raise ServiceValidationError(f"Request failed: {e}")
+
+        if response.status != 200:
+            try:
+                parsed_response = self._resolve_error(url, response)
+                raise ServiceValidationError(parsed_response)
+            except Exception as e:
+                raise ServiceValidationError(e)
+
+        response_data = await response.json()
+        _LOGGER.info(f"Response data: {response_data}")
+        return response_data
+
+    async def fetch(self, url):
+        """Fetch image from url and return image data"""
+        _LOGGER.info(f"Fetching image from {url}")
+        try:
+            response = await self.session.get(url)
+        except Exception as e:
+            raise ServiceValidationError(f"Failed to fetch image: {e}")
+
+        if response.status != 200:
+            raise ServiceValidationError(
+                f"Fetch failed with status code {response.status}")
+
+        data = await response.read()
+        return data
+
+    def _resolve_error(self, url, response):
+        """Translate response status to error message"""
+        if url == ENDPOINT_OPENAI:
+            if response.status == 401:
+                return "Invalid Authentication. Ensure you are using a valid API key."
+            if response.status == 403:
+                return "Country, region, or territory not supported."
+            if response.status == 404:
+                return "The requested model does not exist."
+            if response.status == 429:
+                return "Rate limit exceeded. You are sending requests too quickly."
+            if response.status == 500:
+                return "Issue on OpenAI's servers. Wait a few minutes and try again."
+            if response.status == 503:
+                return "OpenAI's Servers are experiencing high traffic. Try again later."
+            else:
+                return f"Error: {response}"
+        elif url == ENDPOINT_ANTHROPIC:
+            if response.status == 400:
+                return "Invalid Request. There was an issue with the format or content of your request."
+            if response.status == 401:
+                return "Invalid Authentication. Ensure you are using a valid API key."
+            if response.status == 403:
+                return "Access Error. Your API key does not have permission to use the specified resource."
+            if response.status == 404:
+                return "The requested model does not exist."
+            if response.status == 429:
+                return "Rate limit exceeded. You are sending requests too quickly."
+            if response.status == 500:
+                return "Issue on Anthropic's servers. Wait a few minutes and try again."
+            if response.status == 529:
+                return "Anthropic's Servers are experiencing high traffic. Try again later."
+            else:
+                return f"Error: {response}"
+        elif url == ENDPOINT_GOOGLE:
+            if response.status == 400:
+                return "User location is not supported for the API use without a billing account linked."
+            if response.status == 403:
+                return "Access Error. Your API key does not have permission to use the specified resource."
+            if response.status == 404:
+                return "The requested model does not exist."
+            if response.status == 406:
+                return "Insufficient Funds. Ensure you have enough credits to use the service."
+            if response.status == 429:
+                return "Rate limit exceeded. You are sending requests too quickly."
+            if response.status == 503:
+                return "Google's Servers are temporarily overloaded or down. Try again later."
+            else:
+                return f"Error: {response}"
+        elif url == ENDPOINT_OLLAMA:
+            if response.status == 400:
+                return "Invalid Request. There was an issue with the format or content of your request."
+            if response.status == 404:
+                return "The requested model does not exist."
+            if response.status == 500:
+                return "Internal server issue (on Ollama server)."
+            else:
+                return f"Error: {response}"
+        elif url == ENDPOINT_LOCALAI:
+            if response.status == 400:
+                return "Invalid Request. There was an issue with the format or content of your request."
+            if response.status == 404:
+                return "The requested model does not exist."
+            if response.status == 500:
+                return "Internal server issue (on LocalAI server)."
+            else:
+                return f"Error: {response}"
+
+    async def close(self):
+        # Home Assistant will close the session
+        # TODO: There is a warning "Unclosed client session", but if closed it throws an error...
+        # await self.session.close()
+        pass
