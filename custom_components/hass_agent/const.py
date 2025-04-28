875de1257cbe438a8e958813ce03a91ca4e087e2
Danny Tsang <danny@tsang.uk>
2025-04-28 12:03:25 +0100

    Removed HASS Agent integration.

--- custom_components/hass_agent/const.py
+++
@@ -1,7 +0,0 @@
-"""Constants for the HASS.Agent integration."""
-
-DOMAIN = "hass_agent"
-
-CONF_ACTION = "action"
-CONF_DEVICE_NAME = "device_name"
-CONF_DEFAULT_NOTIFICATION_TITLE = "default_notification_title"
